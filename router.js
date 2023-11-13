51153adc6c71ccf5bbbe0b778a71635d599f6ed0
Darshana Wijebahu <117095106+ShanCodeWay@users.noreply.github.com>
2023-11-13 09:40:12 +0530

    do some chnges

--- router.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import inventoryRoutes from './routes/inventory.js';
-
-router.use('/inventory', inventoryRoutes);
-
-export default router;
